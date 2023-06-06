5517b11f5fee3f029fc269599ea4bc83648184a5
Federico Verly <federicoverly@gmail.com>
2023-06-06 15:40:53 +0200

    Fix setup types

--- src/setupProxy.ts
+++ src/setupProxy.ts
@@ -1,5 +1,7 @@
-module.exports = function (app) {
-  app.use(function (_req, res, next) {
+import { Express, Request, Response, NextFunction } from 'express';
+
+module.exports = function (app: Express) {
+  app.use(function (_req: Request, res: Response, next: NextFunction) {
     res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
     res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
     next();
